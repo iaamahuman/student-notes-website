83e047751d44d98dc2218488ed2d64d5cb2bc6fb
iaamahuman <iaamahuman@gmail.com>
2026-03-23 11:06:44 +0530

    Disable middleware - auth handled client side

--- middleware.ts
+++ middleware.ts
@@ -2,29 +2,9 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname;
-
-  const isPublicPath = path === "/login" || path === "/register";
-
-  const token = request.cookies.get("token")?.value || "";
-
-  if (isPublicPath && token) {
-    return NextResponse.redirect(new URL("/", request.nextUrl));
-  }
-
-  if (!isPublicPath && !token) {
-    return NextResponse.redirect(new URL("/login", request.nextUrl));
-  }
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    "/login",
-    "/order",
-    "/order/:orderId*",
-    "/settings",
-    "/register",
-    "/settings/myorders",
-    "/settings/profile",
-  ],
+  matcher: [],
 };
